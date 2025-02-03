7104d0da046b563dc7a7947c996b43e3bfabf698
Charlotte Chen <chenhaob@grinnell.edu>
2025-02-02 23:00:55 -0500

    Update blog content and personal summary; add Fall 2024 semester reflections and new academic details

--- src/App.vue
+++ src/App.vue
@@ -11,22 +11,15 @@ if (import.meta.hot)
   <n-config-provider :theme="isDark ? darkTheme : null" :theme-overrides="isDark ? darkOverrides : null">
     <Header class="mb-4 h-16 box-border" />
     <div class="flex">
-      <Sidebar
-        id="sidebar"
+      <Sidebar id="sidebar"
         class="<lg:fixed <lg:z-3 <lg:right-[-300px] w-[256px] flex-shrink-0 sticky top-20 <lg:top-16 overflow-auto h-full lg:mr-4 duration-300 bg-hex-fff dark:bg-hex-18181c"
-        style="max-height: calc(100vh - 80px)"
-      />
+        style="max-height: calc(100vh - 80px)" />
       <main class="flex-grow min-w-0 pb-10 text-center text-gray-700 dark:text-gray-200">
         <router-view />
-        <div
-          class="lg:hidden mdui-overlay"
-          @click="() => toggleSidebar()"
-        />
+        <div class="lg:hidden mdui-overlay" @click="() => toggleSidebar()" />
       </main>
-      <Toc
-        class="<xl:hidden w-[256px] flex-shrink-0 sticky top-20 overflow-scroll h-full"
-        style="max-height: calc(100vh - 80px)"
-      />
+      <Toc class="<xl:hidden w-[256px] flex-shrink-0 sticky top-20 overflow-scroll h-full"
+        style="max-height: calc(100vh - 80px)" />
     </div>
   </n-config-provider>
 </template>
--- src/pages/about.md
+++ src/pages/about.md
@@ -8,8 +8,11 @@ title: About
 
 ## About me
 
-I'm a student at Columbia University, majoring in Computer Engineering. I'm interested in computer architecture, CPU design, and VLSI design. I'm also a fan of open-source software and Linux.
-
+I'm a student at Columbia University, majoring in Computer Engineering. I'm interested in computer architecture, CPU design, and VLSI design. I'm also interested in classical Japanese, Nintendo switch games, and gender studies.
+<!-- Finish this -->
+PS: *I study classical Japanese because I want to read some offical Japanese government documents.*  
+More PS: *I did bad in my classical Japanese class, but I still love it.*  
+Even More PS: *I will study classical Japanese again in Fall 2025 -> if I have time.*  
 
 ## Contact me
 
@@ -18,7 +21,7 @@ I'm a student at Columbia University, majoring in Computer Engineering. I'm inte
 - Telegram: [CharlotteChen2002](https://t.me/CharlotteChen2002)
 - Email：
   - charlottechendaily2002 AT gmail.com (personal)
-  - hc3558 AT columbia.edu (academic)
+  - chencharl AT grinnell.edu (academic)
 - My anime list: [Bangumi](/bangumi)
 
 
